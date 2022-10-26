a4b9a62dc42b5c890d352bdc1b748ab938a39f77
FN03A078 <825281732@qq.com>
2022-10-26 14:52:46 +0800

    feat: add init command now user can build tasks file and get notice when dose not run 'init' before

--- src/main/java/tw/cn/cap/gtb/todo/App.java
+++ src/main/java/tw/cn/cap/gtb/todo/App.java
@@ -1,11 +1,45 @@
 package tw.cn.cap.gtb.todo;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+
+    private static final String TODO_PATH = System.getProperty("user.home") + File.separator + ".todo";
+    private static final String TASKS_FILE = TODO_PATH + File.separator + "tasks";
+    private static final String INIT_MSG = "Initialized successfully.";
+    private static final String INIT_ERR_MSG = "Please run 'todo init' before running '%s' command.";
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        if (args.length != 0) {
+            App app = new App();
+            if ("init".equals(args[0])) {
+                app.init();
+            } else if (!preCheck()) {
+                //list
+            } else {
+                System.out.printf(INIT_ERR_MSG, args[0]);
+            }
+        }
+    }
+
+    private static boolean preCheck() {
+        return Files.notExists(Path.of(TASKS_FILE));
+    }
+
+    private void init() {
+        try {
+            if (!Files.exists(Path.of(TODO_PATH))) {
+                Files.createDirectories(Path.of(TODO_PATH));
+            }
+            if (!Files.exists(Path.of(TASKS_FILE))) {
+                Files.createFile(Path.of(TASKS_FILE));
+            }
+            System.out.println(INIT_MSG);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
