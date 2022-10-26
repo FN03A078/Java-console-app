4cd2abd96aca1455b649c2949b5e4420fd4b907c
FN03A078 <825281732@qq.com>
2022-10-26 20:30:32 +0800

    refactor: adjust format and add annotation

--- src/main/java/tw/cn/cap/gtb/todo/App.java
+++ src/main/java/tw/cn/cap/gtb/todo/App.java
@@ -17,8 +17,13 @@ public class App {
     private static final String TASKS_FILE = TODO_PATH + File.separator + "tasks";
     private static final String INIT_MSG = "Initialized successfully.";
     private static final String INIT_ERR_MSG = "Please run 'todo init' before running '%s' command.";
+
     //private static final String PARAS_ERR = "";
+
+    //collection used to store records from local file
     private static List<Task> tasks;
+
+    //variable used to indicate the newly added task id
     private static String nextId;
 
 
@@ -41,20 +46,45 @@ public class App {
         }
     }
 
+
+    /**
+     * Check whether the user has executed the "init" command before,
+     * if not, the tasks file will not exist.
+     *
+     * @return if "tasks" file not exists, return true, else false
+     */
     private static boolean preCheck() {
         return Files.notExists(Path.of(TASKS_FILE));
     }
 
+
+    /**
+     * get the paras without command.
+     *
+     * @param args User's Command Line Parameters
+     * @return an array contains the paras without command
+     */
     private String[] getParas(String[] args) {
         return Arrays.stream(args).skip(1).toArray(String[]::new);
     }
 
+
+    /**
+     * get the paras without command then convert to String
+     *
+     * @param args User's Command Line Parameters
+     * @return task name to be added
+     */
     private String getTaskName(String[] args) {
         String[] paras = getParas(args);
         return String.join(" ", paras);
     }
 
 
+    /**
+     * read first line and assign to nextId,
+     * then read remaining lines and store in list "tasks".
+     */
     private void readFile() {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(TASKS_FILE))) {
             nextId = bufferedReader.readLine();
@@ -68,7 +98,13 @@ public class App {
         }
     }
 
-    //isDone id taskName
+
+    /**
+     * Split parma "record" into isDone, id and taskName,
+     * then store in list "tasks".
+     *
+     * @param record Each line read from the file
+     */
     private void storeList(String record) {
         String[] split = record.split("\\s+", 3);
         tasks.add(new Task(
@@ -77,46 +113,11 @@ public class App {
                 split[2]));
     }
 
-    private void init() {
-        try {
-            if (!Files.exists(Path.of(TODO_PATH))) {
-                Files.createDirectories(Path.of(TODO_PATH));
-            }
-            if (!Files.exists(Path.of(TASKS_FILE))) {
-                Files.createFile(Path.of(TASKS_FILE));
-            }
-            System.out.println(INIT_MSG);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private void list() {
-        Map<Boolean, List<Task>> collect =
-                tasks.stream().collect(Collectors.groupingBy(Task::getDone));
-        System.out.println("# To be done");
-        listByStatus(collect, false);
-        System.out.println("# Completed");
-        listByStatus(collect, true);
-    }
-
-    private void listByStatus(Map<Boolean, List<Task>> collect, Boolean flag) {
-        if (collect.get(flag) != null) {
-            for (Task task : collect.get(flag)) {
-                System.out.println(task);
-            }
-        } else {
-            System.out.println("Empty");
-        }
-    }
-
-    private void add(String taskName) {
-        int taskId = nextId == null ? 1 : Integer.parseInt(nextId);
-        nextId = String.valueOf(taskId + 1);
-        tasks.add(new Task(false, taskId, taskName));
-        writeFile();
-    }
-
+    /**
+     * write the nextId at first line of "tasks" file,
+     * Then write the records in "tasks" list to local file.
+     */
     private void writeFile() {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(TASKS_FILE))) {
             bufferedWriter.write(nextId);
@@ -132,36 +133,105 @@ public class App {
     }
 
 
-    private void mark(String[] markIds) {
-        if (markIds.length != 0 && !tasks.isEmpty()) {
-                for (String markId : markIds) {
-                    for (Task task : tasks) {
-                        try {
-                            if (task.getId() == Integer.parseInt(markId)) {
-                                task.setDone(true);
-                            }
-                        } catch (NumberFormatException e) {
-                            //System.out.print(PARAS_ERR);
-                        }
-                    }
-                }
-                writeFile();
-
+    private void listByStatus(Map<Boolean, List<Task>> collect, Boolean flag) {
+        if (collect.get(flag) != null) {
+            for (Task task : collect.get(flag)) {
+                System.out.println(task);
+            }
+        } else {
+            System.out.println("Empty");
         }
     }
 
 
-    private void remove(String[] removeIds) {
-        if (removeIds.length != 0 && !tasks.isEmpty()) {
-                for (String removeId : removeIds) {
+    // command method below
+
+    /**
+     * create "tasks" file then
+     * print message when created successfully.
+     */
+    private void init() {
+        try {
+            if (!Files.exists(Path.of(TODO_PATH))) {
+                Files.createDirectories(Path.of(TODO_PATH));
+            }
+            if (!Files.exists(Path.of(TASKS_FILE))) {
+                Files.createFile(Path.of(TASKS_FILE));
+            }
+            System.out.println(INIT_MSG);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * list all tasks by isDone flag.
+     */
+    private void list() {
+        Map<Boolean, List<Task>> collect =
+                tasks.stream().collect(Collectors.groupingBy(Task::getDone));
+        System.out.println("# To be done");
+        listByStatus(collect, false);
+        System.out.println("# Completed");
+        listByStatus(collect, true);
+    }
+
+
+    /**
+     * add new task which has auto-increment & unique id to list "tasks",
+     * then write tasks file.
+     *
+     * @param taskName entered by the user
+     */
+    private void add(String taskName) {
+        int taskId = nextId == null ? 1 : Integer.parseInt(nextId);
+        nextId = String.valueOf(taskId + 1);
+        tasks.add(new Task(false, taskId, taskName));
+        writeFile();
+    }
+
+
+    /**
+     * mark the finished task.
+     *
+     * @param markIds task ids to be marked
+     */
+    private void mark(String[] markIds) {
+        if (markIds.length != 0 && !tasks.isEmpty()) {
+            for (String markId : markIds) {
+                for (Task task : tasks) {
                     try {
-                        tasks.removeIf(task -> task.getId() == Integer.parseInt(removeId));
+                        if (task.getId() == Integer.parseInt(markId)) {
+                            task.setDone(true);
+                        }
                     } catch (NumberFormatException e) {
                         //System.out.print(PARAS_ERR);
                     }
                 }
-                writeFile();
+            }
+            writeFile();
+
         }
     }
 
+
+    /**
+     * delete tasks from tasks file,
+     * this method will permanently delete the assigned tasks from local file.
+     *
+     * @param removeIds task ids to be removed
+     */
+    private void remove(String[] removeIds) {
+        if (removeIds.length != 0 && !tasks.isEmpty()) {
+            for (String removeId : removeIds) {
+                try {
+                    tasks.removeIf(task -> task.getId() == Integer.parseInt(removeId));
+                } catch (NumberFormatException e) {
+                    //System.out.print(PARAS_ERR);
+                }
+            }
+            writeFile();
+        }
+    }
 }
