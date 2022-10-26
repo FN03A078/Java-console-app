31e567a33156be0ccc6b0896b136420f055ec087
FN03A078 <825281732@qq.com>
2022-10-26 15:14:00 +0800

    feat: add method to read file and store in list

--- src/main/java/tw/cn/cap/gtb/todo/App.java
+++ src/main/java/tw/cn/cap/gtb/todo/App.java
@@ -1,9 +1,15 @@
 package tw.cn.cap.gtb.todo;
 
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class App {
 
@@ -11,6 +17,8 @@ public class App {
     private static final String TASKS_FILE = TODO_PATH + File.separator + "tasks";
     private static final String INIT_MSG = "Initialized successfully.";
     private static final String INIT_ERR_MSG = "Please run 'todo init' before running '%s' command.";
+    private static List<Task> tasks;
+
 
     public static void main(String[] args) {
         if (args.length != 0) {
@@ -29,6 +37,27 @@ public class App {
         return Files.notExists(Path.of(TASKS_FILE));
     }
 
+    private void readFile() {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(TASKS_FILE))) {
+            tasks = new ArrayList<>();
+            String record;
+            while ((record = bufferedReader.readLine()) != null) {
+                storeList(record);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //isDone id taskName
+    private void storeList(String record) {
+        String[] split = record.split("\\s+", 3);
+        tasks.add(new Task(
+                Boolean.parseBoolean(split[0]),
+                Integer.parseInt(split[1]),
+                split[2]));
+    }
+
     private void init() {
         try {
             if (!Files.exists(Path.of(TODO_PATH))) {
@@ -42,4 +71,6 @@ public class App {
             e.printStackTrace();
         }
     }
+
+
 }
